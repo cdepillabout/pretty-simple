89b022a68e41444d659f25cef7844a5726b3959c
Andrew Lei <andrew.w.lei@gmail.com>
2018-07-10 03:38:36 -0400

    Revert "Fix missing space for string literal"
    This reverts commit cedb8a83dc2a1b9c601f8b7213fd04371f903834.

--- src/Text/Pretty/Simple/Internal/ExprToOutput.hs
+++ src/Text/Pretty/Simple/Internal/ExprToOutput.hs
@@ -227,7 +227,7 @@ putExpression (Parens commaSeparated) = do
 putExpression (StringLit string) = do
   nest <- gets nestLevel
   when (nest < 0) $ addToNestLevel 1
-  addOutputs $ [ OutputStringLit string, OutputOther " "]
+  addOutput $ OutputStringLit string
 putExpression (Other string) = do
   nest <- gets nestLevel
   when (nest < 0) $ addToNestLevel 1
