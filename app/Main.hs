43a81ed32048ea1e74a3ee8b92be33b9c95ce6c3
Andrew Lei <andrew.w.lei@gmail.com>
2018-07-10 04:31:47 -0400

    Allow light-bg and no-color modes for executable

--- app/Main.hs
+++ app/Main.hs
@@ -26,10 +26,55 @@ module Main where
 import Data.Text (unpack)
 import qualified Data.Text.IO as T
 import qualified Data.Text.Lazy.IO as LT
-import Text.Pretty.Simple (pString)
+import Options.Applicative 
+       ( Parser, ReadM, execParser, fullDesc, help, helper, info, long
+       , maybeReader, option, progDesc, short, showDefaultWith, value, (<**>))
+import Data.Semigroup ((<>))
+import Text.Pretty.Simple 
+       ( pStringOpt, OutputOptions
+       , defaultOutputOptionsDarkBg
+       , defaultOutputOptionsLightBg
+       , defaultOutputOptionsNoColor
+       )
+
+data Color = DarkBg 
+           | LightBg 
+           | NoColor
+
+newtype Args = Args { color :: Color }
+
+colorReader :: ReadM Color
+colorReader = maybeReader colorReader'
+  where
+    colorReader' "dark-bg"  = Just DarkBg
+    colorReader' "light-bg" = Just LightBg
+    colorReader' "no-color" = Just NoColor
+    colorReader' _          = Nothing
+
+args :: Parser Args
+args = Args 
+    <$> option colorReader
+        ( long "color"
+       <> short 'c'
+       <> help "Select printing color. Available options: dark-bg (default), light-bg, no-color."
+       <> showDefaultWith (\_ -> "dark-bg")
+       <> value DarkBg
+        )
 
 main :: IO ()
 main = do
+  args' <- execParser opts 
   input <- T.getContents
-  let output = pString $ unpack input
+  let printOpt = getPrintOpt $ color args'
+      output = pStringOpt printOpt $ unpack input
   LT.putStr output
+  where
+    opts = info (args <**> helper)
+      ( fullDesc
+     <> progDesc "Format Haskell data types with indentation and highlighting"
+      )
+
+    getPrintOpt :: Color -> OutputOptions
+    getPrintOpt DarkBg  = defaultOutputOptionsDarkBg
+    getPrintOpt LightBg = defaultOutputOptionsLightBg
+    getPrintOpt NoColor = defaultOutputOptionsNoColor
