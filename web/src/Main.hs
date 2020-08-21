a6935c926117e04cdfe9984b8516799f23e0b077
George Thomas <georgefsthomas@gmail.com>
2020-08-21 12:11:07 +0100

    Clean up

--- web/src/Main.hs
+++ web/src/Main.hs
@@ -4,7 +4,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 module Main (main) where
 
@@ -14,19 +13,11 @@ import qualified Network.Wai.Handler.Warp as Warp
 import Network.WebSockets (defaultConnectionOptions)
 #endif
 
-import Control.Monad (join)
 import Control.Monad.State
-import Data.List.NonEmpty (NonEmpty, nonEmpty)
-import Data.Maybe (fromMaybe)
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
+import Data.List.NonEmpty (NonEmpty)
 import Language.Javascript.JSaddle
-import Language.Javascript.JSaddle.String
 import Lens.Micro
-import qualified Lucid as L
 import Prettyprinter
-import Prettyprinter.Render.Text (renderStrict)
-import Prettyprinter.Render.Util.SimpleDocTree
 
 #ifndef __GHCJS__
 runApp :: JSM () -> IO ()
@@ -44,11 +35,11 @@ main = runApp $ do
     doc ^. js ("body" :: JSString)
         ^. jss
             ("innerHTML" :: JSString)
-            (toJSString $ TL.toStrict t1)
+            (toJSString string)
 
-t1 :: TL.Text
-t1 =
-    TL.pack . show . annotateStyle . layoutPretty defaultLayoutOptions $
+string :: String
+string =
+    show . annotateStyle . layoutPretty defaultLayoutOptions $
         annotate Open "(" <> annotate Comma "," <> line <> annotate Close ")"
 
 data Annotation
@@ -56,18 +47,20 @@ data Annotation
     | Close
     | Comma
 
-annotateStyle ds = evalState (traverse style ds) initialTape
-  where
-    style = \case
-        Open -> modify moveR *> gets tapeHead
-        Close -> gets tapeHead <* modify moveL
-        Comma -> gets tapeHead
-    initialTape =
+annotateStyle :: Traversable t => t Annotation -> t ()
+annotateStyle ds =
+    evalState
+        (traverse f ds)
         Tape
             { tapeLeft = streamRepeat ()
             , tapeHead = ()
             , tapeRight = streamCycle $ pure ()
             }
+  where
+    f = \case
+        Open -> modify moveR *> gets tapeHead
+        Close -> gets tapeHead <* modify moveL
+        Comma -> gets tapeHead
 
 -- | A bidirectional Turing-machine tape:
 -- infinite in both directions, with a head pointing to one element.
