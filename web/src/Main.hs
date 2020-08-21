c311a3401a5af7ff5ce9bff2ced5c468ec59bc89
George Thomas <georgefsthomas@gmail.com>
2020-08-21 12:07:18 +0100

    Remove Miso

--- web/src/Main.hs
+++ web/src/Main.hs
@@ -15,14 +15,18 @@ import Network.WebSockets (defaultConnectionOptions)
 #endif
 
 import Control.Monad (join)
+import Control.Monad.State
+import Data.List.NonEmpty (NonEmpty, nonEmpty)
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
-import Miso hiding (go)
-import Miso.String (ms)
+import qualified Data.Text.Lazy as TL
+import Language.Javascript.JSaddle
+import Language.Javascript.JSaddle.String
+import Lens.Micro
+import qualified Lucid as L
 import Prettyprinter
 import Prettyprinter.Render.Text (renderStrict)
 import Prettyprinter.Render.Util.SimpleDocTree
-import Text.Pretty.Simple
-import Text.Pretty.Simple.Internal
 
 #ifndef __GHCJS__
 runApp :: JSM () -> IO ()
@@ -35,50 +39,65 @@ runApp app = app
 #endif
 
 main :: IO ()
-main =
-    runApp $
-        startApp
-            App
-                { initialAction = ()
-                , model = ()
-                , update = \() () -> noEff ()
-                , view = \() -> renderHtml . fmap styleBold . treeForm $ layoutString'
-                , events = defaultEvents
-                , subs = []
-                , logLevel = Off
-                , mountPoint = Nothing
-                }
+main = runApp $ do
+    doc <- jsg ("document" :: JSString)
+    doc ^. js ("body" :: JSString)
+        ^. jss
+            ("innerHTML" :: JSString)
+            (toJSString $ TL.toStrict t1)
 
-layoutString' =
-    annotateStyle opts $
-        layoutPretty
-            defaultLayoutOptions
-            $ annotate Open "(" <> annotate Comma "," <> line <> annotate Close ")"
+t1 :: TL.Text
+t1 =
+    TL.pack . show . annotateStyle . layoutPretty defaultLayoutOptions $
+        annotate Open "(" <> annotate Comma "," <> line <> annotate Close ")"
 
-opts :: OutputOptions
-opts =
-    defaultOutputOptionsNoColor
-        { outputOptionsColorOptions =
-              Just $
-                  ColorOptions
-                      { colorQuote = colorNull
-                      , colorString = colorNull
-                      , colorError = colorNull
-                      , colorNum = colorNull
-                      , colorRainbowParens = []
-                      }
-        }
+data Annotation
+    = Open
+    | Close
+    | Comma
 
--- TODO having the SDS pre-computed means we don't crash
--- as does const fmapping either of the present styles
--- using defaultOutputOptionsNoColor
--- annotating directly with nullColor
+annotateStyle ds = evalState (traverse style ds) initialTape
+  where
+    style = \case
+        Open -> modify moveR *> gets tapeHead
+        Close -> gets tapeHead <* modify moveL
+        Comma -> gets tapeHead
+    initialTape =
+        Tape
+            { tapeLeft = streamRepeat ()
+            , tapeHead = ()
+            , tapeRight = streamCycle $ pure ()
+            }
 
-renderHtml :: SimpleDocTree Bool -> View action
-renderHtml =
-    let go = \case
-            STAnn ann content -> map (if ann then id else id) $ go content
-            STChar c -> [text $ ms $ T.singleton c]
-            STConcat contents -> foldMap go contents
-            _ -> []
-     in pre_ [] . go
+-- | A bidirectional Turing-machine tape:
+-- infinite in both directions, with a head pointing to one element.
+data Tape a = Tape
+    { -- | the side of the 'Tape' left of 'tapeHead'
+      tapeLeft :: Stream a
+    , -- | the focused element
+      tapeHead :: a
+    , -- | the side of the 'Tape' right of 'tapeHead'
+      tapeRight :: Stream a
+    }
+    deriving (Show)
+
+-- | Move the head left
+moveL :: Tape a -> Tape a
+moveL (Tape (l :.. ls) c rs) = Tape ls l (c :.. rs)
+
+-- | Move the head right
+moveR :: Tape a -> Tape a
+moveR (Tape ls c (r :.. rs)) = Tape (c :.. ls) r rs
+
+-- | An infinite list
+data Stream a = a :.. Stream a deriving (Show)
+
+-- | Analogous to 'repeat'
+streamRepeat :: t -> Stream t
+streamRepeat x = x :.. streamRepeat x
+
+-- | Analogous to 'cycle'
+-- While the inferred signature here is more general,
+-- it would diverge on an empty structure
+streamCycle :: NonEmpty a -> Stream a
+streamCycle xs = foldr (:..) (streamCycle xs) xs
